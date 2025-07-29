960cf766df92e2e0fcaf2409d8039b91eb41464f
Penzi_ <101235314+PaoloAJ@users.noreply.github.com>
2025-07-29 10:25:32 -0400

    Task class created

---
+++ src/app/library/scheduler.js
@@ -0,0 +1,9 @@
+class Task {
+  constructor(priority, deadline, name, duration, id) {
+    this.priority = priority; // lower = higher priority
+    this.deadline = new Date(deadline); // converts to a native "date" object in js
+    this.name = name;
+    this.duration = duration; // in seconds or minutes? You guys choose
+    this.id = id;
+  }
+}
