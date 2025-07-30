fbc1179dc6b9a669654d9fe867b5c1401192d18d
Penzi_ <101235314+PaoloAJ@users.noreply.github.com>
2025-07-30 00:38:25 -0400

    Revised task attributes

--- src/app/library/scheduler.js
+++ src/app/library/scheduler.js
@@ -1,11 +1,8 @@
 class Task {
-  constructor(priority, deadline, name, duration, id) {
+  constructor(priority, start, end, name) {
     this.priority = priority; // lower = higher priority
-    this.deadline = new Date(deadline); // converts to a native "date" object in js
+    this.start = start;
+    this.end = end;
     this.name = name;
-    this.duration = duration; // in seconds or minutes? You guys choose
-    this.id = id;
   }
 }
-
-
